e47da4b485bde9ba94fbc617a7285f9c5daeac85
SofiyaHevorhyan <avisventus@gmail.com>
2018-02-24 20:56:14 +0200

    Initial commit

---
+++ examples/url_for_api.py
@@ -0,0 +1,35 @@
+# File: url_rof_api.py
+# A simple program getting the num of watchers of wiki page
+
+import urllib.error
+import urllib.request
+import json
+
+
+def use_url(name):
+    wiki_url = "https://en.wikipedia.org/w/api.php?" \
+               "action=query&" \
+               "titles={0}&" \
+               "prop=info&" \
+               "inprop=watchers&" \
+               "format=json&formatversion=2".format(name.replace(" ", "%20"))
+    connection = urllib.request.urlopen(wiki_url)
+    data = connection.read().decode()
+
+    js = json.loads(data)
+
+    f = open("data.json", "w+", encoding="utf-8")
+    json.dump(js, f, ensure_ascii=False)
+    f.close()
+
+    try:
+        print("Page {0} has {1} "
+              "watchers".format(name, str(js["query"]["pages"][0]["watchers"])))
+    except KeyError:
+        print("Sorry. We have no access to the num of watchers of ", name)
+
+
+use_url("Wikipedia")
+use_url("API:Main page")
+use_url("Google")
+use_url("Programming language")
